ce6fbfb6dd67a8447f13478ef244768d2d34016c
xd <xmandunn@gmail.com>
2019-04-23 14:14:45 -0400

    sync

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-// const apiUrl = 'http://redbadgegroup3-api.herokuapp.com';
-const apiUrl = 'http://127.0.0.1:5000';
+const apiUrl = 'http://redbadgegroup3-api.herokuapp.com';
+// const apiUrl = 'http://127.0.0.1:5000';
 
 @Injectable()
 export class ProfileService {
