4c3cd51e3f55490cb09eaf58162eb56fbf11f2c2
xd <xmandunn@gmail.com>
2019-04-24 15:11:41 -0400

    deploy fixed api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,5 +12,5 @@ switch (window.location.hostname) {
         break;
     default:
         // this is the local host name of your API
-        apiUrl = 'https://redbadgegroup3-api.herokuapp.com/';
+        apiUrl = 'https://redbadgegroup3-api.herokuapp.com';
 }
